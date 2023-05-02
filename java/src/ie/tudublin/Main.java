bffc6e5b8383661587549e0e29241e0c8edceae2
ranahayes <44164165+ranahayes@users.noreply.github.com>
2023-05-02 14:43:17 +0100

    Create Main.java

---
+++ java/src/ie/tudublin/Main.java
@@ -0,0 +1,29 @@
+package ie.tudublin;
+
+import c123456.*;
+
+public class Main
+{
+    
+	public void Menu()
+	{
+		MainVisual visualizer = new MainVisual();
+
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, visualizer);
+		String[] b = {"Second"};
+        processing.core.PApplet.runSketch( b, new Options(visualizer));
+    }
+
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.Menu();
+	}
+}
+
+
+
+
+
